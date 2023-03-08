794ad50267cf3127ef360f07b74bb6c42a4b9fdf
JVanDenDorpe <Jef.vandendorpe@ixor.be>
2023-03-08 10:19:28 +0100

    cleanup licensing

--- src/main/java/be/jvdd/hibernatedemo/repository/AccountRestResource.java
+++ src/main/java/be/jvdd/hibernatedemo/repository/AccountRestResource.java
@@ -1,24 +1,4 @@
-/*
- * 2020 (c) Ixor BV
- * All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Ixor BV
- *
- * The intellectual and technical concepts contained
- * herein are proprietary to Ixor BV
- * and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- *
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Ixor BV.
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.
- */
+
 package be.jvdd.hibernatedemo.repository;
 
 import be.jvdd.hibernatedemo.domain.Account;
@@ -33,7 +13,6 @@ import java.util.List;
 
 @RepositoryRestResource
 public interface AccountRestResource extends PagingAndSortingRepository<Account, Long>, CrudRepository<Account, Long> {
-
     @Query("SELECT DISTINCT account.customerName " +
             "FROM Account account " +
             "WHERE UPPER(account.customerName) LIKE CONCAT('%',UPPER(:filter),'%') " +
--- src/test/java/be/jvdd/hibernatedemo/test/AccountTest.java
+++ src/test/java/be/jvdd/hibernatedemo/test/AccountTest.java
@@ -1,24 +1,3 @@
-/*
- * 2020 (c) Ixor BV
- * All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Ixor BV
- *
- * The intellectual and technical concepts contained
- * herein are proprietary to Ixor BV
- * and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- *
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Ixor BV.
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.
- */
 package be.jvdd.hibernatedemo.test;
 
 import be.jvdd.hibernatedemo.HibernateDemoApplication;
