9393315d4d04fb2785b9fd071f8835dd22310e09
JVanDenDorpe <Jef.vandendorpe@ixor.be>
2023-03-08 10:31:50 +0100

    fix context loads test

--- src/test/java/be/jvdd/hibernatedemo/HibernateDemoApplicationTests.java
+++ src/test/java/be/jvdd/hibernatedemo/HibernateDemoApplicationTests.java
@@ -1,7 +1,6 @@
 package be.jvdd.hibernatedemo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,7 +8,6 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @SpringBootTest(classes = HibernateDemoApplication.class, webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
-@AutoConfigureRestDocs
 class HibernateDemoApplicationTests {
 
 	@Test
